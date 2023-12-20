0a199545f92bf16132bb7df9530acdb0340b63d7
newproject23 <143152902+newproject23@users.noreply.github.com>
2023-12-20 10:08:34 +0300

    Update constraints.py

--- constraints.py
+++ constraints.py
@@ -1,3 +1,3 @@
 API_KEY = "xxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-BITLY_ACCESS_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+BITLY_ACCESS_TOKEN = "5b22e0efb83c140ad41dab9d5cd44d6f6a76146f"
 ngrok_auth_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
