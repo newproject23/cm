82bd7b348482369f6275d9a056b92ab23ee27f41
newproject23 <143152902+newproject23@users.noreply.github.com>
2023-12-20 10:34:08 +0300

    Update zbot.py

--- zbot.py
+++ zbot.py
@@ -73,7 +73,7 @@ try:
 
     @bot.message_handler(commands=["start"])
     def send_start_message(msg):
-        bot.reply_to(msg,"Welcome.....ZCam tool is for Eductaional purpose only. Use /help for more info. Support @Team_ETF for more..... JOIN: https://youtube.com/channel/UCJnx0yDhcTLWM3ZrAtSvaIw")
+        bot.reply_to(msg,"Welcome.....ZCam tool is for Eductaional purpose only. Use /help for more info. Support @source_alpop for more..... JOIN: @j_S_9 -- @vip_alpop")
         global user_id
         user_id = msg.chat.id
 
